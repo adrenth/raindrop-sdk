38ec5b7d38d693ec5cc253dd2e004434194ecf71
Alwin Drenth <adrenth@gmail.com>
2018-05-29 14:28:58 +0200

    Add base classes

---
+++ src/SmartContract.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adrenth\Raindrop;
+
+/**
+ * Class SmartContract
+ *
+ * @package Adrenth\Raindrop
+ */
+class SmartContract
+{
+
+}
---
+++ src/Whitelist.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adrenth\Raindrop;
+
+/**
+ * Class Whitelist
+ *
+ * @package Adrenth\Raindrop\Whitelist
+ */
+class Whitelist
+{
+
+}
